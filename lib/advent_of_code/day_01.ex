fe473a2781bf172c3d74e8d29c49c2474375e621
Daniele Del Giudice <daniele@delgiudice.dev>
2021-12-03 10:16:19 +0100

    Day 01

--- lib/advent_of_code/day_01.ex
+++ lib/advent_of_code/day_01.ex
@@ -1,7 +1,33 @@
 defmodule AdventOfCode.Day01 do
-  def part1(args) do
+  def part1(input) do
+    input = parse(input)
+    do_part1(input)
   end
 
-  def part2(args) do
+  def part2(input) do
+    input = parse(input)
+    do_part2(input)
+  end
+
+  defp parse(input) do
+    input
+    |> String.split()
+    |> Enum.map(&String.to_integer/1)
+  end
+
+  defp do_part1([_ | []]), do: 0
+
+  defp do_part1([a, b | rest]) do
+    count = if a < b, do: 1, else: 0
+    count + do_part1([b | rest])
+  end
+
+  defp do_part2([_ | []]), do: 0
+  defp do_part2([_a, _b | []]), do: 0
+  defp do_part2([_a, _b, _c | []]), do: 0
+
+  defp do_part2([a, b, c, d | rest]) do
+    count = if a < d, do: 1, else: 0
+    count + do_part2([b, c, d | rest])
   end
 end
--- test/advent_of_code/day_01_test.exs
+++ test/advent_of_code/day_01_test.exs
@@ -3,19 +3,30 @@ defmodule AdventOfCode.Day01Test do
 
   import AdventOfCode.Day01
 
-  @tag :skip
+  @input """
+  199
+  200
+  208
+  210
+  200
+  207
+  240
+  269
+  260
+  263
+  """
+
   test "part1" do
-    input = nil
+    input = @input
     result = part1(input)
 
-    assert result
+    assert result == 7
   end
 
-  @tag :skip
   test "part2" do
-    input = nil
+    input = @input
     result = part2(input)
 
-    assert result
+    assert result == 5
   end
 end

853ae3204fbee90c0b4cbfceae10343b9c2cd931
Daniele Del Giudice <daniele@delgiudice.dev>
2021-12-17 01:59:10 +0100

    Day 12

--- lib/advent_of_code/day_12.ex
+++ lib/advent_of_code/day_12.ex
@@ -1,7 +1,64 @@
 defmodule AdventOfCode.Day12 do
-  def part1(_args) do
+  defp parse(input) do
+    input
+    |> String.split(["\r", "\n"], trim: true)
+    |> Enum.map(fn row ->
+      [start, stop] = String.split(row, "-")
+      {start, stop}
+    end)
+    |> Enum.reduce(%{}, fn
+      {start, stop}, acc ->
+        acc
+        |> Map.update(start, [stop], &[stop | &1])
+        |> Map.update(stop, [start], &[start | &1])
+    end)
   end
 
-  def part2(_args) do
+  defp bfs(_, "end", _, path), do: [Enum.reverse(path)]
+
+  defp bfs(map, current, visited, path) do
+    map[current]
+    |> Enum.filter(fn cave ->
+      cave == String.upcase(cave) or !(cave in visited)
+    end)
+    |> Enum.flat_map(fn neigh ->
+      bfs(map, neigh, [neigh | visited], [neigh | path])
+    end)
+  end
+
+  def part1(input) do
+    input
+    |> parse()
+    |> bfs("start", ["start"], ["start"])
+    |> Enum.count()
+  end
+
+  defp bfs_v2(_, "end", _, path, _), do: [Enum.reverse(path)]
+
+  defp bfs_v2(map, current, visited, path, jolly_used) do
+    map[current]
+    |> Enum.map(fn cave ->
+      cond do
+        cave == "start" -> nil
+        cave == String.upcase(cave) -> {cave, jolly_used}
+        !(cave in visited) -> {cave, jolly_used}
+        !jolly_used -> {cave, true}
+        true -> nil
+      end
+    end)
+    |> Enum.filter(fn
+      nil -> false
+      _ -> true
+    end)
+    |> Enum.flat_map(fn {neigh, jolly_used} ->
+      bfs_v2(map, neigh, [neigh | visited], [neigh | path], jolly_used)
+    end)
+  end
+
+  def part2(input) do
+    input
+    |> parse()
+    |> bfs_v2("start", ["start"], ["start"], false)
+    |> Enum.count()
   end
 end
--- test/advent_of_code/day_12_test.exs
+++ test/advent_of_code/day_12_test.exs
@@ -3,19 +3,89 @@ defmodule AdventOfCode.Day12Test do
 
   import AdventOfCode.Day12
 
-  @tag :skip
-  test "part1" do
-    input = nil
+  @input1 """
+  start-A
+  start-b
+  A-c
+  A-b
+  b-d
+  A-end
+  b-end
+  """
+
+  @input2 """
+  dc-end
+  HN-start
+  start-kj
+  dc-start
+  dc-HN
+  LN-dc
+  HN-end
+  kj-sa
+  kj-HN
+  kj-dc
+  """
+
+  @input3 """
+  fs-end
+  he-DX
+  fs-he
+  start-DX
+  pj-DX
+  end-zg
+  zg-sl
+  zg-pj
+  pj-he
+  RW-he
+  fs-DX
+  pj-RW
+  zg-RW
+  start-pj
+  he-WI
+  zg-he
+  pj-fs
+  start-RW
+  """
+
+  test "part1 - input1" do
+    input = @input1
     result = part1(input)
 
-    assert result
+    assert result == 10
   end
 
-  @tag :skip
-  test "part2" do
-    input = nil
+  test "part1 - input2" do
+    input = @input2
+    result = part1(input)
+
+    assert result == 19
+  end
+
+  test "part1 - input3" do
+    input = @input3
+    result = part1(input)
+
+    assert result == 226
+  end
+
+  test "part2 - input1" do
+    input = @input1
     result = part2(input)
 
-    assert result
+    assert result == 36
+  end
+
+  test "part2 - input2" do
+    input = @input2
+    result = part2(input)
+
+    assert result == 103
+  end
+
+  test "part2 - input3" do
+    input = @input3
+    result = part2(input)
+
+    assert result == 3509
   end
 end
